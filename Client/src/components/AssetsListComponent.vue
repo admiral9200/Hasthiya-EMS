6c636f5b0e1f30d6665e2cb925c90e553c7b0431
Nicholas Wong <lion9200.world@gmail.com>
2023-03-28 22:52:12 +0530

    AssetesListComponets#2

--- Client/src/components/AssetsListComponent.vue
+++ Client/src/components/AssetsListComponent.vue
@@ -6,21 +6,37 @@
                 <th class=" screen-md:hidden">Serial Number</th>
                 <th class=" screen-md:hidden">Asset Assignee</th>
                 <th class=" screen-md:hidden">Recived Date</th>
+                <th class=" screen-md:hidden">Edit</th>                
+                <th class=" screen-md:hidden">Delete</th>
             </thead>
             <tbody>
                 <tr class="odd:bg-white even:bg-slate-50" v-for="asset in assets" v-bind:key="asset._id">
                     <td>{{ asset.name }}</td>
                     <td class=" screen-md:hidden ">{{ asset.type.name }}</td>
                     <td class=" screen-md:hidden">{{ asset.serialNumber }}</td>
-                    <td class=" screen-md:hidden">{{ asset.assignPerson.name }}</td>
-                    <td class=" screen-md:hidden">{{ formatDate(asset.updatedAt) }}</td>
+                    <td class=" screen-md:hidden" v-if="asset.assignPerson">{{ asset.assignPerson.name }}</td>
+                    <td class=" screen-md:hidden" v-else>-</td>
+                    <td class=" screen-md:hidden" v-if="asset.assignDate">{{ formatDate(asset.assignDate) }}</td>
+                    <td class=" screen-md:hidden" v-else>-</td>
+                    <td ><i class="fa fa-pen hover:text-green-500"></i>
+                    </td>
+                    <td><i class="fa fa-trash-can hover:text-red-500"></i></td>
+                
                 </tr>
             </tbody>
         </table>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
     props: ['assets'],
+    methods:{
+        formatDate(value) {
+            if (value) {
+                return moment(String(value)).format('MMMM Do YYYY, h:mm a')
+            }
+        },
+    }
 }
 </script>
