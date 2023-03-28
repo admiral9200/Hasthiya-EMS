39ca10f4cb2d803b0e483894e36949d6a72aae43
Nicholas Wong <lion9200.world@gmail.com>
2023-03-28 21:59:33 +0530

    add leave service

---
+++ Client/src/services/LeaveService.js
@@ -0,0 +1,60 @@
+import  axios  from "axios";
+import store from "../store/index";
+import {serverURL} from "../constants/Constants";
+
+axios.interceptors.request.use(req => {
+        let accessToken = store.getters.getUserState.token;
+        req.headers.Authorization = `Bearer ${accessToken}`;
+    console.log('INTERCEPTED');
+    return req;
+},
+(error) => {
+  return Promise.reject(error);
+});
+
+export class LeaveService{
+
+   
+    static getAll(){
+        let dataURL = `${serverURL}/leaves`;
+        return axios.get(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+    static getAllByReportPerson(id,page){
+        let dataURL = `${serverURL}/leaves/reportPerson/${id}?page=${page}`;
+        return axios.get(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+    static getAllByEmployee(id,page){
+        let dataURL = `${serverURL}/leaves/user/${id}?page=${page}`;
+        return axios.get(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+    static getById(id){
+        let dataURL = `${serverURL}/leaves/${id}`;
+        return axios.get(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+    static create(data){
+        let dataURL = `${serverURL}/leaves/`;
+        return axios.post(dataURL,data,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+    static update(data,id){
+        let dataURL = `${serverURL}/leaves/${id}`;
+        return axios.put(dataURL,data,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+    static delete(id){
+        let dataURL = `${serverURL}/leaves/${id}`;
+        return axios.delete(dataURL,{headers: {
+            'Content-Type': 'application/json'
+        }});
+    }
+}
