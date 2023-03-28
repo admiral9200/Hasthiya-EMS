7b1de39f9e2465b9b39d90ddb796cd7cd4e5b247
Nicholas Wong <lion9200.world@gmail.com>
2023-03-28 23:37:02 +0530

    SalaryAdminView#2

---
+++ Client/src/views/SalaryAdminView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class=" justify-between flex my-6 mx-7">
+    <h2 class="text-left text-3xl font-bold tracking-tight text-gray-900">
+      Salaries :
+    </h2>
+    <button
+      class="p-3 text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm text-center">
+      <i class="fa fa-add"></i> Add Salary
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
