19998a6327c2be59b9ffe8dc78fc7078ce99aa7d
Nicholas Wong <lion9200.world@gmail.com>
2023-06-06 20:35:56 +0530

    update user list

--- Client/src/components/UserListComponent.vue
+++ Client/src/components/UserListComponent.vue
@@ -2,13 +2,15 @@
     <ul role="list" class="p-6 divide-y divide-black">
         <li v-for="user in users" v-bind:key="user._id" class="flex py-4 first:pt-0 last:pb-0">
             <div class="w-fit  text-xs">
-                <img class="h-10 w-10 rounded-full float-right"
-                    :src="user.imageurl"
-                    alt="" />
+                <img class="h-10 w-10 rounded-full float-right" :src="user.imageurl" alt="" />
             </div>
             <div class="ml-3 w-full overflow-hidden ">
-                <p class="text-sm font-medium text-slate-900 text-left">{{user.name}}</p>
-                <p class="text-sm text-slate-500 truncate text-left">{{user.designation.name}}</p>
+                <p class="text-sm font-medium text-slate-900 text-left">
+                    <router-link :to="{ name: 'user', params: { id: user._id } }">
+                        {{ user.name }}
+                    </router-link>
+                </p>
+                <p class="text-sm text-slate-500 truncate text-left">{{ user.designation.name }}</p>
             </div>
         </li>
     </ul>
