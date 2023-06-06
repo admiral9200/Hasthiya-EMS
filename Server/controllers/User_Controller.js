a73473f75619cccfbf0134f3f65e32fdf24159a6
Nicholas Wong <lion9200.world@gmail.com>
2023-06-06 20:42:03 +0530

    update user controller

--- Server/controllers/User_Controller.js
+++ Server/controllers/User_Controller.js
@@ -1,49 +1,66 @@
-const mongoose = require("mongoose");
-
 //Imports
 const users = require('../models/User')
 const ResponseService = require("../utils/RresponseService"); // Response service
+const bcrypt = require("bcryptjs"); // Bcrypt for hashing
 
 // Update
-exports.update=(async (req, res) => {
+exports.updatePassword = (async (req, res) => {
+
+  const { password, newpassword } = req.body;
+  const user = await users.findById(req.params.id);
+  const encryptedPassword = await bcrypt.hash(newpassword, 10);
+
+  if (await bcrypt.compare(password, user.password)) {
+    let newuser = { password: encryptedPassword }
+    users.findByIdAndUpdate(req.params.id, newuser, (err, doc) => {
+      ResponseService.generalPayloadResponse(err, doc, res, "password updated successfully");
+    });
+  }else{  
+    return res.json({ status: 400, msg: "Invalid Credentials" });
+  }
+});
+
+// Update
+exports.update = (async (req, res) => {
   users.findByIdAndUpdate(req.params.id, req.body, (err, doc) => {
     ResponseService.generalPayloadResponse(err, doc, res, "user updated successfully");
   });
 });
 
 // Get by id
-exports.getById=(async (req, res) => {
+exports.getById = (async (req, res) => {
   users.findById(req.params.id, (err, doc) => {
     ResponseService.generalPayloadResponse(err, doc, res);
   })
-  .populate('designation', 'name')
-  .populate('technologies', 'name')
+    .populate('designation', 'name')
+    .populate('technologies', 'name')
+    .populate('reportPerson', 'name')
 });
 
 // Soft Delete
-exports.delete=(async(req, res) => {
+exports.delete = (async (req, res) => {
   users.findByIdAndUpdate(req.params.id, (err, doc) => {
     ResponseService.generalPayloadResponse(err, doc, res, "user deleted successfully");
   });
 });
 
 //get all
-exports.getAll=(async(req, res) => {
+exports.getAll = (async (req, res) => {
   // Pagination parameters
   const limit = req.query.limit ? parseInt(req.query.limit) : 10;
   const page = req.query.page ? parseInt(req.query.page) - 1 : 0;
 
 
-  const totalPages = Math.ceil(await users.countDocuments({role: { $ne: 'pending' }}) / limit);
+  const totalPages = Math.ceil(await users.countDocuments({ role: { $ne: 'pending' } }) / limit);
 
-   users.find({role: { $ne: 'pending' }},(err, doc) => {
+  users.find({ role: { $ne: 'pending' } }, (err, doc) => {
     const newPayload = {
       docs: doc,
       totalPages: totalPages
     }
     ResponseService.generalPayloadResponse(err, newPayload, res);
   })
-    .sort({ name: -1 })
+    .sort({ name: 1 })
     .populate('designation', 'name')
     .populate('technologies', 'name')
     .skip(page * limit)
@@ -51,11 +68,11 @@ exports.getAll=(async(req, res) => {
 });
 
 //get all
-exports.getAllForAdmin=(async(req, res) => {
-   users.find((err, doc) => {
+exports.getAllForAdmin = (async (req, res) => {
+  users.find((err, doc) => {
     ResponseService.generalPayloadResponse(err, doc, res);
   })
-    .sort({ createdAt: -1 })
+    .sort({ name: 1 })
     .populate('designation', 'name')
     .populate('technologies', 'name')
 });
@@ -65,20 +82,20 @@ exports.getAllByDesignation = async function (req, res) {
   const designation = req.params.designation;
 
   // Pagination parameters
-  const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+  const limit = req.query.limit ? parseInt(req.query.limit) : 5;
   const page = req.query.page ? parseInt(req.query.page) - 1 : 0;
 
 
   const totalPages = Math.ceil(await users.countDocuments({ designation: designation }) / limit);
 
-   users.find({ designation: designation }, (err, doc) => {
+  users.find({ designation: designation }, (err, doc) => {
     const newPayload = {
       docs: doc,
       totalPages: totalPages
     }
     ResponseService.generalPayloadResponse(err, newPayload, res);
   })
-    .sort({ name: -1 })
+    .sort({ name: 1 })
     .populate('designation', 'name')
     .populate('technologies', 'name')
     .skip(page * limit)
@@ -90,20 +107,20 @@ exports.getAllByTechnology = async function (req, res) {
   const technology = req.params.technology;
 
   // Pagination parameters
-  const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+  const limit = req.query.limit ? parseInt(req.query.limit) : 5;
   const page = req.query.page ? parseInt(req.query.page) - 1 : 0;
 
 
-  const totalPages = Math.ceil(await users.countDocuments({ technologies: technology}) / limit);
+  const totalPages = Math.ceil(await users.countDocuments({ technologies: technology }) / limit);
 
-   users.find({technologies: technology}, (err, doc) => {
+  users.find({ technologies: technology }, (err, doc) => {
     const newPayload = {
       docs: doc,
       totalPages: totalPages
     }
     ResponseService.generalPayloadResponse(err, newPayload, res);
   })
-    .sort({ name: -1 })
+    .sort({ name: 1 })
     .populate('designation', 'name')
     .populate('technologies', 'name')
     .skip(page * limit)
@@ -111,13 +128,13 @@ exports.getAllByTechnology = async function (req, res) {
 }
 
 // search user by name
-exports.searchAllByName = async function (req, res) {  
+exports.searchAllByName = async function (req, res) {
   const searchTerm = req.params.searchTerm;
 
-  const limit = req.query.limit ? parseInt(req.query.limit) : 10;
-  const page = req.query.page ? parseInt(req.query.page) : 0;
+  const limit = req.query.limit ? parseInt(req.query.limit) : 5;
+  const page = req.query.page ? parseInt(req.query.page) - 1 : 0;
 
-  
+
   const totalPages = Math.ceil(await users.countDocuments({ name: { $regex: searchTerm, $options: 'i' } }) / limit);
 
   users.find({ name: { $regex: searchTerm, $options: 'i' } }, (err, doc) => {
@@ -127,7 +144,7 @@ exports.searchAllByName = async function (req, res) {
     }
     ResponseService.generalPayloadResponse(err, newPayload, res);
   })
-    .sort({ name: -1 })
+    .sort({ name: 1 })
     .populate('designation', 'name')
     .populate('technologies', 'name')
     .skip(page * limit)
