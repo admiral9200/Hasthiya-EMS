90b78fc8a69c7bfc14666e842bc96f67c5d895d4
Nicholas Wong <lion9200.world@gmail.com>
2023-06-06 20:41:18 +0530

    update auth controller

--- Server/controllers/Auth_Controller.js
+++ Server/controllers/Auth_Controller.js
@@ -9,19 +9,21 @@ const JWT_SECRET = process.env.JWT_SECRET;
 // Signup
 exports.signUp = async function (req, res) {
   const { name, email, password } = req.body;
-
+  if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
+    return res.json({ status: 422, msg: "Invalid Email!" });
+  }
   const encryptedPassword = await bcrypt.hash(password, 10);
 
   const oldUser = await User.findOne({ email });
   if (oldUser) {
-    return res.json({status: 404, msg: "user already exists" });
-  }else{
+    return res.json({ status: 404, msg: "user already exists" });
+  } else {
     let user = { name, email, password: encryptedPassword, }
 
     new User(user).save((err, doc) => {
       ResponseService.generalPayloadResponse(err, doc, res);
     });
-  }  
+  }
 }
 
 // Login
@@ -30,22 +32,28 @@ exports.login = async function (req, res) {
 
   const user = await User.findOne({ email });
   if (!user) {
-    return res.json({status: 404, msg: "User Not found" });
+    return res.json({ status: 404, msg: "User Not found" });
   }
-  if (user.role=='pending') {
-    return res.json({status: 401, msg: "Please Contact Admin Your account still not active " });
+  if (user.role == 'pending') {
+    return res.json({ status: 401, msg: "Please Contact Admin Your account still not active " });
   }
   if (await bcrypt.compare(password, user.password)) {
     const token = jwt.sign({ email: user.email }, JWT_SECRET, {
-      expiresIn: 86400,
+      expiresIn: 7 * 24 * 60 * 60 * 1000,
     });
     if (res.status(200)) {
-      return res.json({  status: 200, data: token });
+      res.cookie('jwt', token, {
+        httpOnly: true, //accessible only by web server 
+        secure: true, //https
+        sameSite: 'None', //cross-site cookie 
+        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
+      })
+      return res.json({ status: 200, data: token });
     } else {
-      return res.json({  status: 400, msg: "error" });
+      return res.json({ status: 400, msg: "Invalid Credentials" });
     }
   }
-  return res.json({ status: 400, msg: "Invalid Password" });
+  return res.json({ status: 400, msg: "Invalid Credentials" });
 }
 
 exports.getUserDetails = async (req, res) => {
@@ -64,12 +72,12 @@ exports.getUserDetails = async (req, res) => {
     }
 
     const useremail = user.email;
-    User.findOne({ email: useremail })
+    User.findOne({ email: useremail }).populate('designation', 'name').populate('reportPerson', 'name')
       .then((data) => {
         res.send({ status: "ok", data: data });
       })
       .catch((error) => {
         res.send({ status: "error", data: error });
       });
-  } catch (error) {}
+  } catch (error) { }
 }
