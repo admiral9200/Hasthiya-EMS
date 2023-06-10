82270ea3ee04747c86286f9123c8341f312ac8e9
Nicholas Wong <lion9200.world@gmail.com>
2023-06-10 20:12:35 +0530

    update index.js

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -10,6 +10,30 @@ const routes = [
     meta:{auth:true}
   },
   {
+    path: '/user/:id',
+    name: 'user',
+    component: () => import('../views/OtherUserProfileView.vue'),
+    meta:{auth:true}
+  },
+  {
+    path: '/userByTech/:id',
+    name: 'userByTech',
+    component: () => import('../views/UserByTechView.vue'),
+    meta:{auth:true}
+  },
+  {
+    path: '/userByDesignation/:id',
+    name: 'userByDesignation',
+    component: () => import('../views/UserByDesignationView.vue'),
+    meta:{auth:true}
+  },
+  {
+    path: '/userByName/:searchTerm',
+    name: 'userByName',
+    component: () => import('../views/UserByNameView.vue'),
+    meta:{auth:true}
+  },
+  {
     path: '/editProfile',
     name: 'editProfile',
     component: () => import('../views/ProfileEditView.vue'),
@@ -34,6 +58,18 @@ const routes = [
     meta:{auth:true,admin:true}
   },
   {
+    path: '/salary',
+    name: 'salary',
+    component: () => import('../views/SalaryAdminView.vue'),
+    meta:{auth:true,admin:true}
+  },
+  {
+    path: '/salary/:date',
+    name: 'salaryByDate',
+    component: () => import('../views/SalaryAdminView.vue'),
+    meta:{auth:true,admin:true}
+  },
+  {
     path: '/technology',
     name: 'technology',
     component: () => import('../views/TechnologyView.vue'),
@@ -52,9 +88,39 @@ const routes = [
     meta:{auth:true,admin:true}
   },
   {
+    path: '/assetsByType/:id',
+    name: 'assetsByType',
+    component: () => import('../views/AssetsAdminByTypeView'),
+    meta:{auth:true}
+  },
+  {
+    path: '/addassets',
+    name: 'addassets',
+    component: () => import('../views/AssetManageView.vue'),
+    meta:{auth:true}
+  },
+  {
+    path: '/editassets/:AssetId',
+    name: 'editassets',
+    component: () => import('../views/AssetManageView.vue'),
+    meta:{auth:true}
+  },
+  {
     path: '/userAssets',
     name: 'userAssets',
     component: () => import('../views/UserAssetsView.vue'),
+    meta:{auth:true}
+  },
+  {
+    path: '/userLeaves',
+    name: 'userLeaves',
+    component: () => import('../views/UserLeavesView.vue'),
+    meta:{auth:true}
+  },
+  {
+    path: '/userSalary',
+    name: 'userSalary',
+    component: () => import('../views/UserSalaryView.vue'),
     meta:{auth:true}
   },
   {
@@ -69,6 +135,12 @@ const routes = [
     component: () => import('../views/SigninView.vue'),
     meta:{auth:false}
   },
+  {
+    path: '/forgetPassword/:id',
+    name: 'forgetPassword',
+    component: () => import('../views/ForgetPasswordView.vue'),
+    meta:{auth:false}
+  },
 ]
 
 const router = createRouter({
@@ -77,9 +149,9 @@ const router = createRouter({
 });
 
 router.beforeEach((to,from,next)=>{
-  if(to.meta.auth && !store.getters.getUserState.token){
+  if(to.meta.auth && !localStorage.getItem('token')){
     next('/signin')
-  }else if(!to.meta.auth && store.getters.getUserState.token){
+  }else if(!to.meta.auth && localStorage.getItem('token')){
     next('/')
   }else if(to.meta.admin && store.getters.getUserState.user.role !='admin'){
     next('/')
