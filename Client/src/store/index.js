56c8fc2aceabfd048cebb11b41182f268dfd408d
Nicholas Wong <lion9200.world@gmail.com>
2023-06-10 20:13:50 +0530

    update index.js

--- Client/src/store/index.js
+++ Client/src/store/index.js
@@ -1,12 +1,18 @@
 import { createStore, createLogger } from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 import userModule from './modules/user.module'
 import designationModule from './modules/designation.module'
 import technologyModule from './modules/technology.module'
 import assetsTypeModule from './modules/assetsType.module'
 import assetsModule from './modules/assets.module'
+import salaryModule from './modules/salary.module'
+import leaveModule from './modules/leave.module'
 
 const debug = process.env.NODE_ENV !== 'production'
 
+const dataState = createPersistedState({
+  paths: ['userList']
+})
 
 export default createStore({
   modules: {
@@ -15,7 +21,9 @@ export default createStore({
     technologyModule,
     assetsTypeModule,
     assetsModule,
+    salaryModule,
+    leaveModule,
   },
   strict: debug,
-  plugins: debug ? [createLogger()] : []
+  plugins:  debug ?  [dataState][createLogger()] : [] [dataState],
 })
